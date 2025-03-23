dfe32bfe86ee3018eda41f40b298853a2c86a158
kevkev <kevin.nguyenoanh30@gmail.com>
2025-03-23 18:52:17 +0100

    modifs

--- Projet_Click_journeY/retour_paiement.php
+++ Projet_Click_journeY/retour_paiement.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 require 'getapikey.php';
 
 if (isset($_GET['status'])) {
@@ -9,13 +9,28 @@ if (isset($_GET['status'])) {
     $status = $_GET['status'];
     $control = $_GET['control'];
 
-    // Vérifier la validité de la valeur de contrôle
     $api_key = getAPIKey($vendeur);
     $valeur_controle = md5($api_key . "#" . $transaction . "#" . $montant . "#" . $vendeur . "#" . $status . "#");
 
     if ($control == $valeur_controle) {
         if ($status == 'accepted') {
-            header("Location : Page_accueil.php");
+            if (isset($_SESSION['reservation']) && isset($_SESSION['email'])){
+                $fichier = 'utilisateurs.json';
+                if(file_exists($fichier)){
+                    $contenu_fichier = file_get_contents($fichier);
+                    $utilisateurs = json_decode($contenu_fichier, true);
+                }
+
+                foreach($utilisateurs['utilisateurs'] as &$user){
+                    if ($user["email"] == $_SESSION['email']){
+                        $user["reservation"][] = $_SESSION['reservation'];
+                    }
+                }
+                $fichier_encode=json_encode($utilisateurs, JSON_PRETTY_PRINT);
+                file_put_contents($fichier,$fichier_encode);
+                header("Location: Page_accueil.php");
+                exit();
+            }
         } else {
             echo "Le paiement a été refusé. Transaction ID : $transaction";
         }
