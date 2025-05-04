de91158a9dcb2535f215487cabb7df7dbf3d47ef
kevkev <kevin.nguyenoanh30@gmail.com>
2025-05-04 20:35:46 +0200

    modif

--- Projet_Click_journeY/prix_total.js
+++ Projet_Click_journeY/prix_total.js
@@ -1,8 +1,10 @@
 function prix_total(){
     let prix_total = document.getElementById("prix_total");
-    prix_total.textContent = 
+    prix = 
     parseInt(prix_total.dataset.extra) + 
     parseInt(document.getElementById("activite").value) * 60 + 
     parseInt(document.getElementById("cantine").value) * 40 + 
     parseInt(document.getElementById("arcade").value) * 10;
+
+    prix_total.textContent = prix.toString() + "€";
 }
